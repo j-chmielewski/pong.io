655d27f5c229e809379c88bd5869faa6be3e0e62
Jacek Chmielewski <jacek.chmielewski@teonite.com>
2013-06-28 14:00:32 +0200

    works, TODO: tweaks

--- pong-server.js
+++ pong-server.js
@@ -2,6 +2,9 @@ var app = require('http').createServer(handler)
 , io = require('socket.io').listen(app)
 , fs = require('fs');
 
+// logging
+io.set('log level', 1);
+
 app.listen(8080);
 
 function handler (req, res) {
@@ -59,12 +62,12 @@ io.sockets.on('connection', function (socket) {
     	    players[(player.number + 1) % 2].socket.emit('opponent-disconnected');
 	}
 	// remove from games
-//	var i = games.indexOf(players);
-//	games.splice(i);
+	var i = games.indexOf(players);
+	games.splice(i);
 	// stop the ball
-//	ballVector = [0, 0];
+	ballVector = [0, 0]
     });
-*/    
+*/
     // let the game begin...
     var ballVector = [2,1];
     ball = {
@@ -72,37 +75,44 @@ io.sockets.on('connection', function (socket) {
 	'y': fieldHeight / 2
     };
     setInterval(function() {
-	if(players.length == 2) {
+	if(players.length == 2 && player.number == 0) {
 	    if(ball.x <= (playerWidth + ballRadius)) {
 		// player 1 side
 		if(players[0].position > ball.y || (players[0].position + playerHeight) < ball.y) {
 		    // player 1 lost
+		    console.log('player 1 lost - reset ball posigion');
 		    ball.x = fieldWidth/2,
 		    ball.y = fieldHeight/2
 		} 
+		console.log('positive vector.x');
 		ballVector[0] = Math.abs(ballVector[0]);
 	    }
 	    if(ball.x >= (fieldWidth - playerWidth - ballRadius)) {
 		// player 2 side
 		if(players[1].position > ball.y || (players[1].position + playerHeight) < ball.y) {
 		    // player 2 lost
+		    console.log('player 2 lost - reset ball posigion');
 		    ball.x = fieldWidth/2,
 		    ball.y = fieldHeight/2
 		} 
+		console.log('negative vector.x');
 		ballVector[0] = -Math.abs(ballVector[0]);
 	    }
 	    // upper wall
 	    if(ball.y <= ballRadius) {
+		console.log('positive vector.y');
 		ballVector[1] = Math.abs(ballVector[1]);
 	    }
 	    // lower wall
 	    if(ball.y >= (fieldHeight - ballRadius)) {
+		console.log('negative vector.y');
 		ballVector[1] = -Math.abs(ballVector[1]);
 	    }
+	    console.log('player.number: ' + player.number + ', ball.x: ' + ball.x + ', ball.y: ' + ball.y + ', ballVector: ' + ballVector);
 	    ball.x += ballVector[0];
 	    ball.y += ballVector[1];
 	    players[0].socket.emit('ball-position', { 'x': ball.x, 'y': ball.y});
 	    players[1].socket.emit('ball-position', { 'x': ball.x, 'y': ball.y});
 	}
-    }, 10);
+    }, 5);
 });
